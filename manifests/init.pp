e766b25fd687e619050b22bbe73fcdff5ad3325d
Patrik Stenmark <patrik.stenmark@gmail.com>
2013-02-27 11:12:28 +0100

    Upgrade Fx to 19.0

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,6 @@
 class firefox {
   package { 'Firefox':
     provider => 'appdmg',
-    source   => 'http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/latest/mac/en-US/Firefox%2018.0.2.dmg'
+    source   => 'http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/latest/mac/en-US/Firefox%2019.0.dmg'
   }
 }
